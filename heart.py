25d8ef573a9994cc0a7848617e4c0aae88ade830
Adya <adyajha776@gmail.com>
2024-03-26 10:43:05 +0530

    Create Heart Shape

---
+++ heart.py
@@ -0,0 +1,17 @@
+# Create Heart Shape
+import turtle
+t=turtle.Turtle()
+t.title=("Heart")
+screen=turtle.Screen()
+screen.bgcolor("pink")
+t.color("red")
+t.begin_fill()
+t.fillcolor("red")
+t.left(140)
+t.forward(180)
+t.circle(-90,200)
+t.left(120)
+t.circle(-90,200)
+t.forward(180)
+t.end_fill()
+t.hideturtle()
